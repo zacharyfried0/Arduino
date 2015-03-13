890721215a6b97ff45654bacb8a586640211a02f
briangentry <brian.gentry1@gmail.com>
2015-03-13 10:59:36 -0700

    Create Pin and Ode.ino

---
+++ Pin and Ode.ino
@@ -0,0 +1,53 @@
+#include"pitches.h"
+int melody[] = {
+  NOTE_FS4, NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_A4, NOTE_G4, NOTE_FS4, NOTE_E4, NOTE_D4, NOTE_D4, NOTE_E4, NOTE_FS4, NOTE_FS4, NOTE_E4, NOTE_E4,
+  NOTE_FS4, NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_A4, NOTE_G4, NOTE_FS4, NOTE_E4, NOTE_D4, NOTE_D4, NOTE_E4, NOTE_FS4, NOTE_E4, NOTE_D4, NOTE_D4,
+  NOTE_E4, NOTE_E4, NOTE_FS4, NOTE_D4, NOTE_E4, NOTE_FS4, NOTE_G4, NOTE_FS4, NOTE_D4, NOTE_E4, NOTE_FS4, NOTE_G4, NOTE_FS4, NOTE_E4, NOTE_D4, NOTE_E4, NOTE_A3,
+  NOTE_FS4, NOTE_FS4, NOTE_G4, NOTE_A4, NOTE_A4, NOTE_G4, NOTE_FS4, NOTE_E4, NOTE_D4, NOTE_D4, NOTE_E4, NOTE_FS4, NOTE_E4, NOTE_D4, NOTE_D4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 8/3, 8, 2,
+  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 8/3, 8, 2,
+  4, 4, 4, 4, 4, 8, 8, 4, 4, 4, 8, 8, 4, 4, 4, 4, 2,
+  4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 8/3, 8, 2
+};
+
+void setup() {
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+}
+
+int note(int note) {
+   int notes[] = {NOTE_A3, NOTE_D4, NOTE_E4, NOTE_FS4, NOTE_G4, NOTE_A4};
+   for (int n = 0; n < 6; n++) {
+     if (melody[note] == notes[n]){
+       return n;
+     }
+  }
+}
+
+void noPin(int pin) {
+  digitalWrite(pin, HIGH);
+}
+
+void killpin(int pin){
+  digitalWrite(pin, LOW);
+}
+
+void loop() {
+  for (int thisNote = 0; thisNote < 62; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    noPin(1 + note(thisNote));
+    tone(8, melody[thisNote], noteDuration); //confusing
+    int pauseBetweenNotes = noteDuration * 1.10;
+    delay(pauseBetweenNotes - 50 );
+    killpin(1 + note(thisNote));
+    delay(50);
+  }
+}
